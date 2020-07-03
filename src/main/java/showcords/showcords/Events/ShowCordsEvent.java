e988b8570e2f737e2d559851a31ebb3904438093
Rczerwiec <czerwiecr1999@gmail.com>
2020-07-03 12:14:02 +0200

    0002-Event and Task added, event is not working

--- src/main/java/showcords/showcords/Events/ShowCordsEvent.java
+++ src/main/java/showcords/showcords/Events/ShowCordsEvent.java
@@ -1,4 +1,20 @@
 package showcords.showcords.Events;
 
-public class ShowCordsEvent {
+import org.bukkit.Bukkit;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.Plugin;
+
+public class ShowCordsEvent implements Listener {
+
+    public void PlayerMoveEvent(PlayerMoveEvent e){
+        Bukkit.getServer().getScheduler().runTaskTimer((Plugin) this, new Runnable() {
+            @Override
+            public void run() {
+                Bukkit.broadcastMessage("Cordinates: x:"+e.getPlayer().getLocation().getX()+" z:"+e.getPlayer().getLocation().getZ());
+            }
+        },0L,100L);
+
+    }
+
 }
--- src/main/java/showcords/showcords/ShowCords.java
+++ src/main/java/showcords/showcords/ShowCords.java
@@ -2,6 +2,7 @@ package showcords.showcords;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
+import showcords.showcords.Events.ShowCordsEvent;
 import showcords.showcords.Tasks.ShowCordsTask;
 
 public final class ShowCords extends JavaPlugin {
@@ -9,7 +10,7 @@ public final class ShowCords extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-        BukkitTask ShowCordsTask = new ShowCordsTask(this).runTaskTimer(this,20L,20L);
+        getServer().getPluginManager().registerEvents(new ShowCordsEvent(),this);
     }
 
     @Override
