1a22863ee298d03151b14a77d4fb6e685fedc4a1
Rczerwiec <czerwiecr1999@gmail.com>
2020-07-03 15:14:31 +0200

    0007-First working version - deleted events, used only runnable in main class

--- src/main/java/showcords/showcords/ShowCords.java
+++ src/main/java/showcords/showcords/ShowCords.java
@@ -1,20 +1,40 @@
 package showcords.showcords;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitTask;
-import showcords.showcords.Events.ShowCordsEvent;
-import showcords.showcords.Tasks.ShowCordsTask;
+import org.bukkit.scheduler.BukkitRunnable;
 
-public final class ShowCords extends JavaPlugin {
+import java.util.List;
+
+public final class ShowCords extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getServer().getPluginManager().registerEvents(new ShowCordsEvent(this),this);
+        runnable();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void runnable(){
+        new BukkitRunnable(){
+
+            @Override
+            public void run() {
+
+                List<Player> players = getServer().getWorld("world").getPlayers();
+                for(Player p : players){
+                    //Bukkit.broadcastMessage("Player:"+p.getName());
+                    if(p.getName().equals("Stylowy")){
+                        Bukkit.broadcastMessage("Cordinates: x:"+p.getLocation().getX()+" z:"+p.getLocation().getZ());
+                    }
+                }
+            }
+        }.runTaskTimer(this,0,3000L);
     }
 }
