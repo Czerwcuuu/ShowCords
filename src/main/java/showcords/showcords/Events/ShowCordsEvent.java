96c47355527f3f5f83482cb8ec10a9de9d19ac36
Rczerwiec <czerwiecr1999@gmail.com>
2020-07-03 12:26:37 +0200

    0003-Added EventHandler, there is next problem with event

--- src/main/java/showcords/showcords/Events/ShowCordsEvent.java
+++ src/main/java/showcords/showcords/Events/ShowCordsEvent.java
@@ -1,13 +1,15 @@
 package showcords.showcords.Events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.Plugin;
 
 public class ShowCordsEvent implements Listener {
 
-    public void PlayerMoveEvent(PlayerMoveEvent e){
+    @EventHandler
+    public void WhenPlayerMoves(PlayerMoveEvent e){
         Bukkit.getServer().getScheduler().runTaskTimer((Plugin) this, new Runnable() {
             @Override
             public void run() {
