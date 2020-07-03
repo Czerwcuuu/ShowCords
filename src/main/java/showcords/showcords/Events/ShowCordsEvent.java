0504334f46e66cddab09bf82e116c03dae2c760a
Rczerwiec <czerwiecr1999@gmail.com>
2020-07-03 12:59:22 +0200

    0006-Tried some fixes

--- src/main/java/showcords/showcords/Events/ShowCordsEvent.java
+++ src/main/java/showcords/showcords/Events/ShowCordsEvent.java
@@ -1,10 +1,12 @@
 package showcords.showcords.Events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 import showcords.showcords.ShowCords;
 
 public class ShowCordsEvent implements Listener {
@@ -17,13 +19,17 @@ public class ShowCordsEvent implements Listener {
 
     @EventHandler
     public void WhenPlayerMoves(PlayerMoveEvent e){
-        Bukkit.getServer().getScheduler().runTaskTimer(plugin, new Runnable() {
-            @Override
-            public void run() {
-                Bukkit.broadcastMessage("Cordinates: x:"+e.getPlayer().getLocation().getX()+" z:"+e.getPlayer().getLocation().getZ());
-            }
-        },1000,20L);
-
+        timer(e.getPlayer());
     }
 
+    public void timer(Player player){
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                Bukkit.broadcastMessage("Cordinates: x:"+player.getLocation().getX()+" z:"+player.getLocation().getZ());
+            }
+        }.runTaskTimer(plugin,100,100);
+    }
 }
+
+
